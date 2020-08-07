631cc88feb68002beb58ff888df18f98fec1d541
Ben Cail <benjamin_cail@brown.edu>
2020-08-07 10:05:15 -0400

    initial commit - basic jetty embedding

---
+++ README.md
@@ -0,0 +1,4 @@
+OCFL-JAVA-HTTP
+==============
+
+Lightweight HTTP layer for an OCFL repository.
---
+++ src/main/java/edu/brown/library/repository/OcflHttp.java
@@ -0,0 +1,31 @@
+package edu.brown.library.repository;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.server.Request;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+public class OcflHttp extends AbstractHandler {
+
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+            throws IOException
+    {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello World</h1>");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8000);
+        server.setHandler(new OcflHttp());
+        server.start();
+        server.join();
+    }
+
+}
