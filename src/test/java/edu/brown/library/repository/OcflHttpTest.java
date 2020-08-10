35834f9ff88b1daf10f06c1135f3bae70bc7efdf
Ben Cail <benjamin_cail@brown.edu>
2020-08-10 11:17:20 -0400

    update test to start a jetty server and hit it with a request

--- src/test/java/edu/brown/library/repository/OcflHttpTest.java
+++ src/test/java/edu/brown/library/repository/OcflHttpTest.java
@@ -1,14 +1,29 @@
 package edu.brown.library.repository;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.URI;
+import java.net.http.HttpResponse;
+
+import org.eclipse.jetty.server.Server;
 import org.junit.jupiter.api.Test;
 
 
 public class OcflHttpTest {
 
     @Test
-    public void testRoot() {
-        var handler = new OcflHttp();
-        var output = handler.getRootOutput();
-        assert output.toString().equals("{\"OCFL ROOT\":\"/tmp\"}");
+    public void testRoot() throws Exception {
+        var server = new Server(8000);
+        server.setHandler(new OcflHttp());
+        server.start();
+
+        var url = "http://localhost:8000/";
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(URI.create(url)).build();
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assert response.statusCode() == 200;
+        var body = response.body();
+        assert body.equals("{\"OCFL ROOT\":\"/tmp\"}");
     }
 }
