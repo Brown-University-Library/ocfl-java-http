2d5d3a75beee7803f2f797aa9a291cabc1a103d7
Ben Cail <benjamin_cail@brown.edu>
2020-08-10 13:50:31 -0400

    tests: setup and stop server before and after each test; use junit Assertions; add a test for 404

--- src/test/java/edu/brown/library/repository/OcflHttpTest.java
+++ src/test/java/edu/brown/library/repository/OcflHttpTest.java
@@ -7,23 +7,44 @@ import java.net.http.HttpResponse;
 
 import org.eclipse.jetty.server.Server;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 
 
 public class OcflHttpTest {
 
-    @Test
-    public void testRoot() throws Exception {
-        var server = new Server(8000);
+    Server server;
+
+    @BeforeEach
+    private void setupServer() throws Exception {
+        server = new Server(8000);
         server.setHandler(new OcflHttp());
         server.start();
+    }
 
+    @AfterEach
+    private void stopServer() throws Exception {
+        server.stop();
+    }
+
+    @Test
+    public void testRoot() throws Exception {
         var url = "http://localhost:8000/";
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(URI.create(url)).build();
         var response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        assert response.statusCode() == 200;
+        Assertions.assertEquals(200, response.statusCode());
         var body = response.body();
-        assert body.equals("{\"OCFL ROOT\":\"/tmp\"}");
+        Assertions.assertEquals("{\"OCFL ROOT\":\"/tmp\"}", body);
+    }
+
+    @Test
+    public void testNotFound() throws Exception {
+        var url = "http://localhost:8000/not-found";
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(URI.create(url)).build();
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        Assertions.assertEquals(404, response.statusCode());
     }
 }
